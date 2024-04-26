5e4c0fc3ef51bef65caa6edeeadd3a051112a65c
angelstxrm <danilbelenkin07@gmail.com>
2024-04-26 17:59:53 +0300

    replace views on Viewset

--- project/urls.py
+++ project/urls.py
@@ -16,15 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import AngelView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from users.views import TaskAPIList, TaskAPIDetailView
+from users.views import TaskViewSet
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'task', TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/tasklist/', TaskAPIList.as_view()),
-    # path('api/v1/tasklist/<int:pk>/', TaskAPIUpdate.as_view()),
-    path('api/v1/taskdetail/<int:pk>/', TaskAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls)), # 127.0.0.1:8000/api/v1/task/
+    
+
+
+    # path('api/v1/tasklist/', TaskViewSet.as_view({'get': 'list'})),
+    # path('api/v1/tasklist/<int:pk>/', TaskViewSet.as_view({'put': 'update'})),
+
+
+
+
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'), 
@@ -34,5 +45,4 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
 
 
-    path('', AngelView.as_view()),
 ]
